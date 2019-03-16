526ac4696a3981909394db66f07d3b98cb4677df
Mai-AboElmaaty <43862201+Mai-AboElmaaty@users.noreply.github.com>
2019-03-16 03:11:14 +0200

    add DIO driver >> functions

--- MCAL.h
+++ MCAL.h
@@ -4,12 +4,19 @@
 #include "stdint.h"
 
 typedef enum {PORT_PIN_IN = 0, PORT_PIN_OUT = 1} Port_PinDirectionType;
+typedef enum {STD_LOW = 0, STD_HIGH = 1} Dio_levelType;
 
 /*MCAL Drivers*/
 
+//PORT driver
 void Port_Init( uint8_t  port_index);
 void Port_SetPinDirection (uint8_t port_index, uint8_t pins_mask, Port_PinDirectionType pins_direction );
 void Port_SetPinPullUp (uint8_t port_index,uint8_t pins_mask,uint8_t enable);
 void Port_SetPinPullDown (uint8_t port_index, uint8_t pins_mask, uint8_t enable);
+
+//DIO driver
+int DIO_ReadPort(uint8_t port_index, uint8_t pins_mask);
+void DIO_WritePort(uint8_t port_index, uint8_t pins_mask, Dio_levelType pins_level);
+void DIO_FlipPort(uint8_t port_index, uint8_t pins_mask);
 
 #endif
